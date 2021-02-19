88452c79f964b97f00e7f4f0dfaf97df492cd42a
Gustavo <gmpinatti@gmail.com>
2021-02-19 11:56:42 -0300

    prepares

---
+++ GymAPI/GymAPI/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GymAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private UserService service; 
+    }
+}
---
+++ GymAPI/GymAPI/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GymAPI.Models
+{
+    public class UserModel
+    {
+        Guid id;
+        string name;
+
+    }
+}
